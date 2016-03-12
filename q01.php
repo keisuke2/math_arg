2b4b2bedf2248a79bd8af423ceefb93b19cb0787
keisuke <keisuke4201@gmail.com>
2016-03-12 13:42:10 +0900

    first commit

---
+++ q01.php
@@ -0,0 +1,57 @@
+<?php 
+
+
+//$num[]=11;
+
+
+//echo $num;
+
+  for($num = 11 ; $num < 600; ++$num) { 
+
+
+ 
+	/*
+	$num2=(array)$num;
+	var_dump($num2);
+	*/
+	//$fruits = array("lemon", "orange", "banana", "apple");
+	//sort($fruits);
+	foreach ($num as $key) {
+
+	if($num == sort($num))
+	{
+		 echo $key;
+		  echo "\n";
+
+	}
+   
+}
+
+
+
+                    
+            }
+
+
+
+
+
+/*
+
+while(true){
+/*
+	if($num == array_flip($num))
+	{
+		//print_r($num);
+
+	}
+	//decoct() 
+
+$num[]=++$num;
+print_r($num);
+*/
+
+
+//}
+
+?>
