934f4fadd8bfd03b7bb1bb1259fb511a76157c74
keisuke <keisuke4201@gmail.com>
2016-03-24 21:24:53 +0900

    20160324

--- start_arg/sample03.java
+++ start_arg/sample03.java
@@ -1,9 +1,13 @@
+import java.util.*;
 public class sample03{
 	public static void main(String[] args){
 		int[] arrayD={35, 50, 50, 60, 50 ,35};
 		//int[] num = new int[]{35, 50, 50, 60, 50 ,35};
+		/*
 		int[] arrayH;
 		arrayH = new int[11];
+		*/
+		int[] arrayH={0,0,0,0,0,0,0,0,0,0,0};
 
 		for(int i=0;i<arrayD.length-1;i++){
 			int k=arrayD[i]%11;
@@ -16,18 +20,21 @@ public class sample03{
 				}
 			}
 		}
-		//System.out.println(arrayH);
-	int x=36;
-	int k=x%11;
-	while(true){
-			if(arrayH[k]==x){
-			System.out.println("格納場所は");
-			System.out.println(k);
-			System.out.println("番目");
-			break;
-			}else{
-				k=(k+1)%11;	//もう一度次の要素が空いていないか確かめる	
-			}
-		}
-		}
+System.out.println(Arrays.toString(arrayH));
+		int x=36;
+		int k=x%11;
+		while(true){
+				if(arrayH[k]==x){
+				System.out.println("格納場所は");
+				System.out.println(k);
+				System.out.println("番目");
+				break;
+				}else{
+					k=(k+1)%11;	//もう一度次の要素が空いていないか確かめる	
+				}
+		}	
+	}
 }
+
+
+//配列の値を出力する。参考　https://teratail.com/questions/533
--- start_arg/sample0_05.php
+++ start_arg/sample0_05.php
@@ -4,7 +4,6 @@
 5つのデータのうち一番大きいのはどれか
 */
 $array=array(12,13,11,14,10);
-
 $max=$array[0];
 for($i=1;$i<count($array)-1;$i++){
 	
@@ -13,5 +12,4 @@ for($i=1;$i<count($array)-1;$i++){
 	}
 }
 echo $max;
-
 ?>
