cf9bc40fb1c2df761d29e915d28763edd6231eba
keisuke <keisuke4201@gmail.com>
2016-04-25 21:11:26 +0900

    20160425

--- start_arg/sample06.php
+++ start_arg/sample06.php
@@ -1,8 +1,5 @@
 <?php
-
-
 $array=array(5,3,4,1);
-
 //添字1番目のデータの大きさを調べる
 if($array[0]>$array[1]){
 	$w=$array[0];
@@ -10,7 +7,6 @@ if($array[0]>$array[1]){
 	$array[1]=$w;
 }
 print_r($array);//3,5,4,1
-
 //添字2番目のデータの大きさを調べる
 if($array[0]>$array[2]){
 	$w=$array[0];
@@ -23,7 +19,6 @@ if($array[1]>$array[2]){
 	$array[2]=$w;
 }
 print_r($array);//3,4,5,1
-
 //添字3番目のデータの大きさを調べる
 if($array[0]>$array[3]){
 	$w=$array[0];
@@ -41,8 +36,5 @@ if($array[2]>$array[3]){
 	$array[3]=$w;
 }
 print_r($array);//1,3,4,5
-
-
-
 ?>
 
